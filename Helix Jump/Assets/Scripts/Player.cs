5756411850b9bbbc2ffe4c0c24d4610af93e54ce
Viacheslav Born <64132803+TABASCO1990@users.noreply.github.com>
2021-12-09 16:56:28 +0500

    AddProject

---
+++ Helix Jump/Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Assets.Scipts;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float BounceSpeed;
+    public Rigidbody Rigidbody;
+    public Game Game;
+    public Platform CurrentPlatform;
+    public AudioSource audioBallBouncePlatform, audioBreakPlatform;
+    
+
+    //Прыгаем вверх
+    public void Bounce()
+    {
+        Rigidbody.velocity = new Vector3(0, BounceSpeed, 0);
+        audioBallBouncePlatform.Play();
+    }
+
+    public void Die()
+    {
+        Game.OnPlayerDied();
+        Rigidbody.velocity = Vector3.zero;
+    }
+
+    public void ReachFinish()
+    {
+        Game.OnPlayerReachedFinish();
+        Rigidbody.velocity = Vector3.zero;
+        
+    }
+}
